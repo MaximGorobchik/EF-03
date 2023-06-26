4a8bed0672ab1890f2e7b0f2aa96dbaa5e0ffad7
GorobchikMaxim <pupezm@gmail.com>
2023-06-26 08:56:40 +0200

    Commit#1

---
+++ LAB EF-03/BillsPayment.Data/BillsPaymentSystemContext.cs
@@ -0,0 +1,30 @@
+﻿namespace P01_BillsPaymentSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using P01_BillsPaymentSystem.Data.EntityConfig;
+    using P01_BillsPaymentSystem.Data.Models;
+    public class BillsPaymentSystemContext : DbContext
+    {
+        public BillsPaymentSystemContext() { }
+        public BillsPaymentSystemContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CreditCard> CreditCards { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseSqlServer(Configuration.connectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new BankAccountConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new CreditCardConfiguration());
+            builder.ApplyConfiguration(new PaymentMethodConfiguration());
+        }
+    }
+}
---
+++ LAB EF-03/BillsPayment.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿
+namespace P01_BillsPaymentSystem.Data
+{
+    internal class Configuration
+    {
+        internal const string connectionString = @"Server=FSB\MYSERVER;Database=BillsPaymentSystem;Integrated Security=True";
+    }
+}
